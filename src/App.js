a5d60600af707db9b9c6d8addfd3af4f1464f8ad
kremlevmax <kremlevmax1989@gmail.com>
2021-11-06 12:48:08 -0700

    Navbar beginning

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import Navbar from "./components/Navbar/Navbar";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className='App'>
+      <Routes>
+        <Route exact path='/' element={<App />} />
+      </Routes>
       <Navbar />
     </div>
   );
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,17 @@
-import React from "react";
-import styles from "./Navbar.module.css";
+import React, { useState } from "react";
 
 const Navbar = () => {
+  const [clicked, setClicked] = useState(false);
+
+  const onClickHandler = () => setClicked(!clicked);
   return (
     <>
-      <nav className={styles.navbar}>
-        <div className={styles.logoContainer}>
+      <nav className='navbar'>
+        <div className='logo-container'>
           Max Kremlev <i className='fas fa-rocket'></i>
+        </div>
+        <div className='menu-icon' onClick={onClickHandler}>
+          <i className={clicked ? "fas fa-times" : "fas fa-bars"} />{" "}
         </div>
       </nav>
     </>
